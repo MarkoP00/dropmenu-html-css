22c7ef430206950e714d30d6591d4f2aadc2d9bb
MarkoP00 <135166893+MarkoP00@users.noreply.github.com>
2024-10-14 21:53:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let navigation = document.querySelector(".navigation");
+
+document.querySelector(".menuToggle").addEventListener("click", function () {
+  this.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
